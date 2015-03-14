73df4753ed6a384e5ec8b592539883bd034e88dd
Kevin Mulvey <kmulvey@linux.com>
2015-03-14 15:38:05 -0400

    docs

--- hamming.go
+++ hamming.go
@@ -1,11 +1,14 @@
 package hamming
 
+// This function returns the hamming distance (int) for two strings
+// returns -1 if the input is not of equal length
 func Calc(one string, two string) int {
 	// check length and bail
 	if len(one) != len(two) {
 		return -1
 	}
 
+	// dont loop if they are the same
 	if one == two {
 		return 0
 	}
